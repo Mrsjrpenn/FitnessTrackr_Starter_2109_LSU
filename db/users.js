4bc1224314ebb9dd163ba9c6cad384f14b211d79
Jessica Brown <jessicabrown@Jessicas-Air.lan>
2021-11-17 10:11:36 -0600

    added getUserById function

--- db/users.js
+++ db/users.js
@@ -1,40 +1,51 @@
 const client = require("./client");
-const bcrypt = require("bcrypt")
-const SALT_COUNT = 10
+const bcrypt = require("bcrypt");
+const SALT_COUNT = 10;
 // database functions
 
 // user functions
 async function createUser({ username, password }) {
-    try {
+  try {
     const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
 
-    const { rows: [user] } = await client.query(`
+    const {
+      rows: [user],
+    } = await client.query(
+      `
     INSERT INTO users(username, password)
     VALUES($1, $2)
     ON CONFLICT (username) DO NOTHING
     RETURNING id, username;
-    `,[username, hashedPassword]);
+    `,
+      [username, hashedPassword]
+    );
     return user;
-    } catch (error){
-      console.error(error)
-    }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-async function getUser({ username, password }) {
-
-}
+async function getUser({ username, password }) {}
 
 async function getUserById(userId) {
-
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+ SELECT id,username
+ FROM users
+ WHERE id=${userId}`);
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-async function getUserByUsername(userName) {
-
-}
+async function getUserByUsername(userName) {}
 
 module.exports = {
   createUser,
   getUser,
   getUserById,
   getUserByUsername,
-}
+};
