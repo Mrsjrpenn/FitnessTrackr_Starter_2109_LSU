14bde45b0ea86a704267435cb661757171045469
Jessica Brown <jessicabrown@Jessicas-Air.lan>
2021-11-19 17:49:27 -0600

    added getAllPublicRoutines, update routines,getRoutinesWithoutActivities,and started getAllRoutinesByUsers and destroyRoutines

--- db/routines.js
+++ db/routines.js
@@ -18,7 +18,17 @@ async function getRoutineById(id) {
   }
 }
 
-async function getRoutinesWithoutActivities() {}
+async function getRoutinesWithoutActivities() {
+  try {
+    const { rows } = await client.query(`
+    SELECT* FROM routines;
+
+    `);
+    return rows;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function getAllRoutines() {
   try {
@@ -44,7 +54,7 @@ async function getAllRoutines() {
     LEFT JOIN activities
     ON routine_activities."activityId" = activities.id;
     `);
-    
+
     const fixedRoutines = {};
 
     const getActivityFromRow = (row) => {
@@ -55,28 +65,27 @@ async function getAllRoutines() {
         duration: row.duration,
         count: row.count,
         routineId: row.routineId,
-        routineActivityId: row.routineActivityId
-      }
-    }
+        routineActivityId: row.routineActivityId,
+      };
+    };
 
-
-    for(const row of rows) {
-      if(fixedRoutines[row.routineId]){
-        fixedRoutines[row.routineId].activities.push(getActivityFromRow(row))
+    for (const row of rows) {
+      if (fixedRoutines[row.routineId]) {
+        fixedRoutines[row.routineId].activities.push(getActivityFromRow(row));
       } else {
-        fixedRoutines[row.routineId] ={
+        fixedRoutines[row.routineId] = {
           id: row.routineId,
           isPublic: row.isPublic,
           name: row.name,
           goal: row.goal,
           creatorId: row.creatorId,
           creatorName: row.creatorName,
-          activities: row.activityId ? [getActivityFromRow(row)] : []
-        }
+          activities: row.activityId ? [getActivityFromRow(row)] : [],
+        };
       }
     }
 
-    const routines = Object.values(fixedRoutines)
+    const routines = Object.values(fixedRoutines);
 
     return routines;
   } catch (error) {
@@ -84,11 +93,102 @@ async function getAllRoutines() {
   }
 }
 
-async function getAllRoutinesByUser({ username }) {}
+async function getAllRoutinesByUser({ username }) {
+  try {
+    const { rows } = await client.query(`
+    SELECT
+    routines.id as "routineId",
+      routines."isPublic",
+      routines.name,
+      routines.goal,
+      routines."creatorId",
+      users.username as "creatorName",
+      routine_activities.id as "routineActivityId",
+      routine_activities.duration,
+      routine_activities.count,
+      activities.id as "activityId",
+      activities.name as "activityName",
+      activities.description as "activityDescription"
+    FROM routines
+    LEFT JOIN users
+    ON routines."creatorId" = users.id
+    LEFT JOIN routine_activities
+    ON routine_activities."routineId" = routines.id
+    LEFT JOIN activities
+    ON routine_activities."activityId" = activities.id
+    
+    `);
+    return rows;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function getPublicRoutinesByUser({ username }) {}
 
-async function getAllPublicRoutines() {}
+async function getAllPublicRoutines() {
+  try {
+    const { rows } = await client.query(`
+    SELECT
+    routines.id as "routineId",
+      routines."isPublic",
+      routines.name,
+      routines.goal,
+      routines."creatorId",
+      users.username as "creatorName",
+      routine_activities.id as "routineActivityId",
+      routine_activities.duration,
+      routine_activities.count,
+      activities.id as "activityId",
+      activities.name as "activityName",
+      activities.description as "activityDescription"
+    FROM routines
+    LEFT JOIN users
+    ON routines."creatorId" = users.id
+    LEFT JOIN routine_activities
+    ON routine_activities."routineId" = routines.id
+    LEFT JOIN activities
+    ON routine_activities."activityId" = activities.id
+    WHERE "isPublic"= true
+    `);
+    console.log("these are the rows", rows);
+    const fixedRoutines = {};
+
+    const getActivityFromRow = (row) => {
+      return {
+        id: row.activityId,
+        name: row.activityName,
+        description: row.activityDescription,
+        duration: row.duration,
+        count: row.count,
+        routineId: row.routineId,
+        routineActivityId: row.routineActivityId,
+      };
+    };
+
+    for (const row of rows) {
+      if (fixedRoutines[row.routineId]) {
+        fixedRoutines[row.routineId].activities.push(getActivityFromRow(row));
+      } else {
+        fixedRoutines[row.routineId] = {
+          id: row.routineId,
+          isPublic: row.isPublic,
+          name: row.name,
+          goal: row.goal,
+          creatorId: row.creatorId,
+          creatorName: row.creatorName,
+          activities: row.activityId ? [getActivityFromRow(row)] : [],
+        };
+      }
+    }
+
+    const routines = Object.values(fixedRoutines);
+
+    return routines;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function getPublicRoutinesByActivity({ id }) {}
 
@@ -111,9 +211,45 @@ async function createRoutine({ creatorId, isPublic, name, goal }) {
   }
 }
 
-async function updateRoutine({ id, ...fields }) {}
+async function updateRoutine({ id, ...fields }) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+  try {
+    const {
+      rows: [routine],
+    } = await client.query(
+      `
+UPDATE routines
+SET ${setString}
+WHERE id= ${id}
+RETURNING *;
+`,
+      Object.values(fields)
+    );
+    return routine;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-async function destroyRoutine(id) {}
+async function destroyRoutine(id) {
+  try {
+    const {
+      rows: [routine],
+    } = await client.query(
+      `
+   DELETE FROM routines
+   WHERE id=$1
+   RETURNING *;
+   `,
+      [id]
+    );
+    return routine;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 module.exports = {
   getRoutineById,
