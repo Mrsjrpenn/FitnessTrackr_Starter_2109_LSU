2211723aa8dcbf3421d729df33b9eca33f87b20c
Barry Richard II <barrypaulrichard@gmail.com>
2021-11-19 14:59:50 -0600

    got getAllRoutines to pass its tests

--- db/routines.js
+++ db/routines.js
@@ -23,10 +23,62 @@ async function getRoutinesWithoutActivities() {}
 async function getAllRoutines() {
   try {
     const { rows } = await client.query(`
-      SELECT *
-      FROM routines;
+    SELECT 
+      routines.id as "routineId",
+      routines."isPublic",
+      routines.name,
+      routines.goal,
+      routines."creatorId",
+      users.username as "creatorName",
+      routine_activities.id as "routineActivityId",
+      routine_activities.duration,
+      routine_activities.count,
+      activities.id as "activityId",
+      activities.name as "activityName",
+      activities.description as "activityDescription"
+    FROM routines
+    LEFT JOIN users
+    ON routines."creatorId" = users.id
+    LEFT JOIN routine_activities
+    ON routine_activities."routineId" = routines.id
+    LEFT JOIN activities
+    ON routine_activities."activityId" = activities.id;
     `);
-    return rows;
+    
+    const fixedRoutines = {};
+
+    const getActivityFromRow = (row) => {
+      return {
+        id: row.activityId,
+        name: row.activityName,
+        description: row.activityDescription,
+        duration: row.duration,
+        count: row.count,
+        routineId: row.routineId,
+        routineActivityId: row.routineActivityId
+      }
+    }
+
+
+    for(const row of rows) {
+      if(fixedRoutines[row.routineId]){
+        fixedRoutines[row.routineId].activities.push(getActivityFromRow(row))
+      } else {
+        fixedRoutines[row.routineId] ={
+          id: row.routineId,
+          isPublic: row.isPublic,
+          name: row.name,
+          goal: row.goal,
+          creatorId: row.creatorId,
+          creatorName: row.creatorName,
+          activities: row.activityId ? [getActivityFromRow(row)] : []
+        }
+      }
+    }
+
+    const routines = Object.values(fixedRoutines)
+
+    return routines;
   } catch (error) {
     console.error(error);
   }
@@ -41,7 +93,6 @@ async function getAllPublicRoutines() {}
 async function getPublicRoutinesByActivity({ id }) {}
 
 async function createRoutine({ creatorId, isPublic, name, goal }) {
-  console.log("inside createRoutine function");
   try {
     const {
       rows: [routine],
@@ -54,7 +105,6 @@ async function createRoutine({ creatorId, isPublic, name, goal }) {
     `,
       [creatorId, isPublic, name, goal]
     );
-    console.log("this is the routine", routine);
     return routine;
   } catch (error) {
     console.error(error);
