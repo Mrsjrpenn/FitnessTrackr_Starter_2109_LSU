905c37e1e9fcefc16e8fdb33853be1fafd696f00
Barry Richard II <barrypaulrichard@gmail.com>
2021-11-22 12:57:13 -0600

    working on setup for front-end

---
+++ Front_End/src/components/App.js
@@ -0,0 +1,10 @@
+
+
+const App = () => {
+  // components go here
+  return (
+    <h1>Hello world</h1>
+  )
+}
+
+export default App;
--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require("path");
+const {PORT = 4000} = process.env;
 
 // Setup your Middleware and API Router here
+
+app.use(express.static(path.join(__dirname, 'Front_End', 'build')));
+app.get('*', (req, res, next) => res.sendFile(path.join(__dirname, 'Front_End', 'build', 'index.html')));
 
 module.exports = app;
