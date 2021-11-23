4be3399462b48f012a24a5988e466caa14f213c0
Barry Richard II <barrypaulrichard@gmail.com>
2021-11-22 18:04:22 -0600

    still fixing heroku

--- Front_End/src/components/App.js
+++ Front_End/src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 
 const App = () => {
   // components go here
   return (
     <main>
-      <NavBar />
+      <Router>
+        <NavBar />
+      </Router>
     </main>
   );
 };
