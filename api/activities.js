93f0f13cefeed297d0a9284bd7161b900844f30a
Jessica Brown <jessicabrown@Jessicas-Air.lan>
2021-11-24 18:43:33 -0600

    added express server routes and need to work on bugs

--- api/activities.js
+++ api/activities.js
@@ -1,5 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+// const { requireUser } = require("../db/users");
+const {
+  createActivities,
+  updateActivities,
+  getAllActivities,
+} = require("../db/activities");
 
 // GET /api/activities/:activityId/routines
 
@@ -8,5 +14,46 @@ const router = express.Router();
 // POST /api/activities
 
 // PATCH /api/activities/:activityId
+
+router.use((req, res, next) => {
+  console.log("Barry and I are making a request to the router");
+  next();
+});
+
+router.get("/activities", async (req, res) => {
+  try {
+    console.log("Calling");
+    const response = await getAllActivities();
+    console.log("HERE IS THE RESPONSE", response);
+    res.send(response);
+  } catch (error) {
+    throw error;
+  }
+});
+router.post("/api/activities", async (req, res) => {
+  const { name, description } = req.body;
+  const activity = {};
+  try {
+    activity.name = name;
+    activity.description = description;
+    const newActivity = await createActivities(activity);
+    if (newActivity) {
+      res.send(newActivity);
+    }
+  } catch (error) {
+    throw error;
+  }
+});
+router.patch("/api/activities/:activityId", async (req, res) => {
+  const { activityId } = req.params;
+  try {
+    const updatedActivities = await updateActivities({ activityId });
+    if (updatedActivities) {
+      res.send(updatedActivities);
+    }
+  } catch (error) {
+    throw error;
+  }
+});
 
 module.exports = router;
--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-
 // Setup your Middleware and API Router here
+const apiRouter = require("./api");
+app.use("/api", apiRouter);
 
-app.use(express.static(path.join(__dirname, 'Front_End', 'build')));
-app.get('*', (req, res, next) => res.sendFile(path.join(__dirname, 'Front_End', 'build', 'index.html')));
+app.use(express.static(path.join(__dirname, "Front_End", "build")));
+app.get("*", (req, res, next) =>
+  res.sendFile(path.join(__dirname, "Front_End", "build", "index.html"))
+);
 
 module.exports = app;
